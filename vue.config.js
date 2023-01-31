c12788df3b202e8b4a2aecc74134d897ffdf38ba
Jaisri5987 <jaisri.ec19@bitsathy.ac.in>
2023-01-31 11:12:29 +0530

    vue configuration file is added

--- vue.config.js
+++ vue.config.js
@@ -2,9 +2,8 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   devServer: {
-    proxy: "http://10.30.1.107:8081"
+    proxy: "http://192.168.52.29:8081"
     //10.30.1.107
     //192.168.52.29
-    
   }
 })
